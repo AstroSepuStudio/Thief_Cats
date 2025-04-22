568c9f146e697a74ee164825b96d3d88090aca30
Sepu <astrosepustudio@gmail.com>
2025-04-22 19:13:24 -0400

    Player and camera movement in singleplayer

---
+++ Assets/_Scripts/PlayerData.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerData : MonoBehaviour
+{
+    public PlayerInput Player_Input;
+    public Transform Player_Character;
+}
---
+++ Assets/_Scripts/SP_CameraMovement.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+public class SP_CameraMovement : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] PlayerData _pData;
+    [SerializeField] Transform _cameraTarget;
+
+    [Header("Values")]
+    [SerializeField] float _sensitivity;
+
+    float _mouseX;
+    float _mouseY;
+
+    float _xRotation = 0f;
+    float _yRotation = 0f;
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void Update()
+    {
+        _mouseX = Input.GetAxis("Mouse X") * _sensitivity * Time.deltaTime;
+        _mouseY = Input.GetAxis("Mouse Y") * _sensitivity * Time.deltaTime;
+    }
+
+    private void LateUpdate()
+    {
+        _xRotation -= _mouseY;
+        _xRotation = Mathf.Clamp(_xRotation, -80f, 80f);
+
+        _yRotation += _mouseX;
+
+        transform.SetPositionAndRotation(_cameraTarget.position, Quaternion.Euler(_xRotation, _yRotation, 0f));
+        _pData.Player_Character.rotation = Quaternion.Euler(0f, _yRotation, 0f);
+    }
+}
